7ea0f6c816707771ff8dcc0ee173c6f57d184e78
Fry-Fr <ryjlwdg89@gmail.com>
2021-04-17 02:16:55 -0400

    form scaffolding

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import Header from "./Header"
 import Home from "./Home"
 
+const initialFormData = {
+  name:'',
+  size:'',
+  toppings:[],
+  specialInstructions:''
+}
+
 const App = () => {
+  
+  const [ formData, setFormData ] = useState(initialFormData)
+
+  const updateForm = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
   return (
     <Route path="/">
       <Header/>
-      <Home/>
+      <Home update={updateForm} form={formData}/>
     </Route>
   );
 };
